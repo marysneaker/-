782bbe64329a1965bb8c697d674b544066a49442
sinoswr <sinoswr@jeffersontekimacbook-pro.local>
2016-02-15 09:42:21 +0800

    Initial Commit

---
+++ 本地推送/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  本地推送
+//
+//  Created by sinoswr on 16/2/15.
+//  Copyright © 2016年 sinoswr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 本地推送/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  本地推送
+//
+//  Created by sinoswr on 16/2/15.
+//  Copyright © 2016年 sinoswr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
